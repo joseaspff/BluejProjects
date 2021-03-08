2a12625187239c62200380eebb51af606916c33f
joseaspff <jose.aspff@gmail.com>
2021-03-08 19:57:02 +0000

    Add files via upload

---
+++ POO_github/Enumerados/Size.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Size - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Size
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
